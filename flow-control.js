32c0d60821559ef06c8f11907ea097f99dfae997
Stephen Wool <stevewool7@gmail.com>
2019-02-26 15:33:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ function teenager(age) {
 
 function ageChecker(age) {
  if(age >= 13 && age <= 19) {
-    return "You are a teenager"
+    return "You are a teenager!"
  } else if(age < 13) {
    return "You are a kid"
  } else if (age > 19) {
@@ -23,7 +23,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
- return age >= 13 && age <= 19 ? "You are a teenager!" : "You are not a teenager"
+ return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
